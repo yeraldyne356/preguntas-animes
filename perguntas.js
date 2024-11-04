6982880af5fa5f4fe9173db83054c912f2858a1b
yeraldyne356 <aylen.quispe@escola.pr.gov.br>
2024-11-04 11:52:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+criaCartao(
+    'animes',
+    'quaia são os melhores animes de romance',
+    'horimiya,Insomniacs After School,Meu Casamento Feliz,Tomo-chan Is a Girl!'
+    
+)
+
+criaCartao(
+    'animes',
+    'quem criu o anime',
+    'Junichi Kouchi e Seitaro Kitayama'
+)
+
+criaCartao(
+    'anime',
+    'em que ano foi criado o anime',
+    '1960 no japon'
+)
+
+criaCartao(
+    'anime',
+    'qual e o anime mais reconhecido mundialmente',
+    'dragon ball'
+)
+
+criaCartao(
+    'anime',
+    'qual e o anime mais longo',
+    'Sazae-san , tem mais de 7.000 episodios'
+)
+
+criaCartao(
+    'anime',
+    'quais são os melhores animes de ação',
+    'demon slayer, Bocchi the Rock, Chainsaw Man, One Piece, Jujutsu Kaisen'
+)
